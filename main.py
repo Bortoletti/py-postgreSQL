bd2c7b39bb2d2459e36bbeb3d9246cc890ccea20
Bortoletti <lbortoletti@deltasystems.com.br>
2023-05-22 21:57:08 -0300

    Consultas por Cursos

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import psycopg2
+import json
 
-conn = psycopg2.connect( host="191.252.0.35", database="bigdata", user="delta" , password="Delta202301"  )
+conn = psycopg2.connect( host="", database="", user="" , password=""  )
 
 print( conn.status )
 
 print( conn.info )
 
 with conn.cursor() as cur:
-    cur.execute( 'select * from cnae limit 10')
+    cur.execute( 'select * from cnae limit 10' )
     for record in cur:
        print(record)
+       print( json.dumps( record ))
        print( record[0])
+       cnae = { "codigo": record[0], "nome" : record[1] }
+       print( cnae )
+
 
 conn.close()
 
